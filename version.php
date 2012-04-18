a38023679a5ccc58be6e0f698683a30e776486b6
Juan Leyva <juanleyvadelgado@gmail.com>
2012-04-18 14:35:34 +0300

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2011121703;
+$plugin->version  = 2011121704;
 $plugin->requires = 2010112400;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 2.0;
