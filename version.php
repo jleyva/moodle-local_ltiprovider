a5cf840cdff9144dadfe368c09e5b1c8f6545ed5
Juan Leyva <juanleyvadelgado@gmail.com>
2013-03-11 14:56:32 +0100

    Bump version number

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2011121705;
+$plugin->version  = 2011121706;
 $plugin->requires = 2010112400;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 2.1;
+$plugin->release = 2.2;
