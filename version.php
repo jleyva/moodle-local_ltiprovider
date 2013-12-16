c489a4de9a31cb418f317060674d63db368e0adf
Juan Leyva <juanleyvadelgado@gmail.com>
2013-12-16 15:51:42 +0100

    Fixing version numbers for releasing

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2011121709;
+$plugin->version  = 2011121707;
 $plugin->requires = 2010112400;
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = 2.4;
+$plugin->release = 2.3;
