5c2b834aea57bf5ff007af9fafaff4fd4b9eb9d3
Juan Leyva <juanleyvadelgado@gmail.com>
2014-01-23 16:59:14 +0100

    Bump version for releasing

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2011121708;
+$plugin->version  = 2011121709;
 $plugin->requires = 2011120500; // require Moodle version (2.2).
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release  = '2.3.1';
