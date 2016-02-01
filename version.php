c2ff56c1e96a5e4d3d6353c5d9d7982181490e6b
Juan Leyva <juanleyvadelgado@gmail.com>
2016-02-01 15:25:17 +0100

    Bump version number for version compatible with Moodle 2.9 and Moodle 3.0

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2014080103;
+$plugin->version  = 2016020100;
 $plugin->requires = 2014051200; // Require Moodle version (2.7).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release  = '2.7.1';
+$plugin->release  = '2.9.0';
 $plugin->component = 'local_ltiprovider';
